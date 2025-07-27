2c6416f10064c331fbf9c8422f6522dbb2deff5a
CHANDRU P <chandrupalanisamyooo@gmail.com>
2025-07-27 08:46:56 +0530

    Add files via upload

---
+++ CALCULATOR/script.js
@@ -0,0 +1,37 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("light-theme");
+}
+document.addEventListener("keydown", function (e) {
+  const allowed = "0123456789+-*/%.";
+  if (allowed.includes(e.key)) {
+    appendValue(e.key);
+  } else if (e.key === "Enter") {
+    calculate();
+  } else if (e.key === "Backspace") {
+    deleteLast();
+  } else if (e.key === "Escape") {
+    clearDisplay();
+  }
+});
